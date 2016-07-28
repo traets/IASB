34cb47fb4171aecc3e7c9f7b6c2ad4f7c39bdffa
Frits Traets <frits.traets@kuleuven.be>
2016-07-28 16:24:13 +0200

    put functions outside the importance sampling function

--- choice/R/importance_sampling.R
+++ choice/R/importance_sampling.R
@@ -1,3 +1,75 @@
+#' log Posterior
+#'
+#' Calculates the logposterior with a normal prior density
+#' @param par parameter value(s) for wich the logposterior is calculated
+#' @param design The Design matrix
+#' @param Y the response vector
+#' @param n_alts The number of alternatives in each choice set
+#' @param prior_mode vector containing the prior mode
+#' @param prior_covar matrix containing the prior covariance
+#' @return the logposterior probability
+logPost<-function(par, design, Y, n_alts){
+
+  des<-design
+  p<-t(t(des) * par)
+  p<-.rowSums(p, m= nrow(des), n=length(par))
+  expp<-exp(p)
+  p<-expp/rep(rowsum(expp, rep(seq(1, nrow(des)/n_alts, 1), each = n_alts)), each=n_alts)
+
+  log_L<-sum(Y*log(p))
+
+  logprior2=-0.5*(par -t(prior_mode))%*%solve(prior_covar)%*%(as.matrix(par) - prior_mode)
+
+  logpost<-(length(prior_mode)/2*log(2*pi)-0.5*log(det(prior_covar))) + logprior2 + log_L
+
+  return(logpost)
+
+}
+
+#' Hessian
+#'
+#' @param par parameter value(s)
+#' @param design The design matrix
+#' @param covar the covariance matrix
+#' @param n_alts The number of alternatives in each choice set
+#' @return the hessian matrix
+hessian<-function (par, design, covar, n_alts){
+
+  des<-as.matrix(design)
+  p<- des %*% diag(par)
+  p<-.rowSums(p, m= nrow(des), n=length(par))
+  expp<-exp(p)
+  p<-expp/rep(rowsum(expp, rep(seq(1, nrow(des)/n_alts, 1), each = n_alts)), each=n_alts)
+
+  info<-crossprod(des*p, des) - crossprod(rowsum(des*p, rep(seq(1, nrow(des)/n_alts, 1), each=n_alts)))
+
+  hess<-(-info - solve(covar))
+
+  return(hess)
+
+}
+
+#' Likelihood function
+#'
+#' @param par parameter value(s)
+#' @param design The design matrix
+#' @param n_alts The number of alternatives in each choice set
+#' @param Y the response vector
+#' @return the likelihood
+Lik<-function(par, design, n_alts, Y){
+
+  des<-as.matrix(design)
+
+  p<-t(t(des) * par)
+  p<-.rowSums(p, m= nrow(des), n=length(par))
+  expp<-exp(p)
+  p<-expp/rep(rowsum(expp, rep(seq(1, nrow(des)/n_alts, 1), each = n_alts)), each=n_alts)
+
+  L<-prod(p^Y)
+
+  return(L)
+
+}
 
 #' Importance sampling
 #'
@@ -15,43 +87,6 @@ imp_sampling <- function (prior_mode, prior_covar, design,  n_alts, Y, ...){
 
   #cte
   prior1<-(2*pi)^(-length(mode)/2)*(det(prior_covar))^(-0.5)
-  lp1<-length(prior_mode)/2*log(2*pi)-0.5*log(det(prior_covar))
-
-  #log Posterior
-  logPost<-function(par, design, Y, n_alts, logprior1=lp1){
-
-    des<-design
-    p<-t(t(des) * par)
-    p<-.rowSums(p, m= nrow(des), n=length(par))
-    expp<-exp(p)
-    p<-expp/rep(rowsum(expp, rep(seq(1, nrow(des)/n_alts, 1), each = n_alts)), each=n_alts)
-
-    log_L<-sum(Y*log(p))
-
-    logprior2=-0.5*(par -t(prior_mode))%*%solve(prior_covar)%*%(as.matrix(par) - prior_mode)
-
-    logpost<-logprior1 + logprior2 + log_L
-
-    return(logpost)
-
-  }
-
-  #Hessian
-  hessian<-function (par, design, covar, n_alts){
-
-    des<-as.matrix(design)
-    p<- des %*% diag(par)
-    p<-.rowSums(p, m= nrow(des), n=length(par))
-    expp<-exp(p)
-    p<-expp/rep(rowsum(expp, rep(seq(1, nrow(des)/n_alts, 1), each = n_alts)), each=n_alts)
-
-    info<-crossprod(des*p, des) - crossprod(rowsum(des*p, rep(seq(1, nrow(des)/n_alts, 1), each=n_alts)))
-
-    hess<-(-info - solve(covar))
-
-    return(hess)
-
-  }
 
   #estimate importance mode
   imp_mode<-maxLik::maxNR(logPost, start= prior_mode, design=des, Y=Y, n_alts=n_alts)$estimate
@@ -60,22 +95,6 @@ imp_sampling <- function (prior_mode, prior_covar, design,  n_alts, Y, ...){
   H<-hessian(par = imp_mode, design = des, covar = prior_covar, n_alts = n_alts)
   g_covar<--solve(H)
   g_draws<-lattice_mvt(mode=imp_mode, cvar = g_covar, df=length(imp_mode), ...)
-
-  #likelihood function
-  Lik<-function(par, design, n_alts, Y){
-
-    des<-as.matrix(design)
-
-    p<-t(t(des) * par)
-    p<-.rowSums(p, m= nrow(des), n=length(par))
-    expp<-exp(p)
-    p<-expp/rep(rowsum(expp, rep(seq(1, nrow(des)/n_alts, 1), each = n_alts)), each=n_alts)
-
-    L<-prod(p^Y)
-
-    return(L)
-
-  }
 
   #importance density
   g_dens<-function (par, g_mode, g_covar, df=length(imp_mode)){
@@ -95,10 +114,8 @@ imp_sampling <- function (prior_mode, prior_covar, design,  n_alts, Y, ...){
 
     #prior
     prior[r]<-prior1*exp(-0.5* (g_draws[r, ]-prior_mode) %*% solve(prior_covar) %*% as.matrix(g_draws[r, ]- prior_mode))
-
     #likelihood
     LK[r]<-Lik(par = g_draws[r, ], design=des, Y=Y, n_alts = n_alts)
-
     #density of g
     dens_g[r]<-g_dens(par = g_draws[r, ], g_mode = imp_mode, g_covar = g_covar)
 
