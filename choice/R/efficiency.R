51d22a5937b8085f1c8304108d48d4297724882a
Frits Traets <frits.traets@kuleuven.be>
2016-08-31 16:35:40 +0200

    include Kullback Leibler criterion

--- choice/R/efficiency.R
+++ choice/R/efficiency.R
@@ -61,3 +61,66 @@ info_design<-function (par, design, n_alts){
 
   info_design<- crossprod(des*p, des) - crossprod(rowsum( des*p, group))
 }
+
+
+#' Kullback-Leibler information criterion
+#'
+#' Calcultates the most effecient choice set by maximizing the Kullback-Leibler
+#' distance between the current posterior of the coefficients, and the updated posterior
+#' one would obtain by receiving a response on the next choice set.
+#' @param lvls  A vector which contains for each attribute, the number of levels.
+#' @param par_samples A matrix in which each row is a sample from a multivariate prior parameter distribution.
+#' @param weights A vector containing the weights of all the samples.
+#' @param n_alts Numeric value indicating the number of alternatives per choice set.
+#' @export
+KL_info<- function (lvls, par_samples, weights, n_alts){
+
+  #All choice sets, without same profile twice
+  fp<-profiles(lvls)
+  fcomb<-full_sets(cand = fp, n_alts = n_alts)
+  rows<-apply(fcomb[, 1:ncol(fcomb)], 1, function(i) length(unique(i)) > ncol(fcomb)-1)
+  fcomb<-fcomb[rows, ]
+  minKL<-0
+
+  for (s in 1:nrow(fcomb)){
+
+    #take set
+    set<-as.matrix(fp[as.numeric(fcomb[s, ]), ])
+
+    #probability
+    num<-set%*%t(par_samples)
+    mmat<-as.matrix(t(apply(num[, 1:ncol(num)], 2, max)))
+    nummax<-exp(sweep(num, MARGIN=2, mmat, FUN="-"))
+    denom<-colSums(nummax)
+
+    probs<-sweep(nummax, MARGIN=2, denom, FUN="/")
+    logprobs<-log(probs)
+
+    wprob<- sweep(probs, MARGIN=2, weights, FUN="*")
+    totwprob<- rowSums(wprob)
+    logwprob<- sweep(logprobs, MARGIN=2, weights, FUN="*")
+    totlogwprob<- rowSums(logwprob)
+
+    #kullback Leibler information
+    klinfo<- 0
+    for( a in 1:n_alts){
+      klinfo<- klinfo + (totwprob[a] * (log(totwprob[a]) - totlogwprob[a]))
+    }
+
+    if (klinfo > minKL){
+      minKL<- klinfo
+      new_set<- set
+    }
+  }
+
+  return (new_set)
+}
+
+
+
+
+
+
+
+
+
