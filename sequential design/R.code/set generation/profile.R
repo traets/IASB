a575b851131c4db4af9879b8e670ecf61fc185b8
Frits Traets <frits.traets@kuleuven.be>
2017-02-27 11:20:39 +0100

    remove folder seq des

--- sequential design/R.code/set generation/profile.R
+++
@@ -1,7 +0,0 @@
-##profile
-
-Rprof("profile1.out", line.profiling=TRUE)
-eval(parse(file = "C:/Users/u0105757/Desktop/sequential_design.sas/R.code/IASB_R/DBerror_MODfederov.9.0.R", keep.source=TRUE))
-Rprof(NULL)
-
-summaryRprof("profile1.out", lines = "show")
